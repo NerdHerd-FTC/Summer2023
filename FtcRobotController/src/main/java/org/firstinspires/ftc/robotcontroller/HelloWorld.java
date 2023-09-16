a2d4c017c422590b0f828dadad3ec7987cea2e0f
Aanya1234 <85971907+Aanya1234@users.noreply.github.com>
2023-09-15 17:26:45 -0700

    Update HelloWorld.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/HelloWorld.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/HelloWorld.java
@@ -8,25 +8,25 @@ public class HelloWorld extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        telemetry.addLine("Hello world");
+        telemetry.addLine("Hello world this is Aanya");
         telemetry.update();
 
         waitForStart();
         int count = 0;
         while (opModeIsActive()) {
             count++;
-            telemetry.addData("Millenia", count/60/60/24/7/4/12/10/10/10);
-            telemetry.addData("Centuries", count/60/60/24/7/4/12/10/10);
-            telemetry.addData("Decades", count/60/60/24/7/4/12/10);
+            telemetry.addData("Millenia", count/60/60/24/7/4/12/10/10/10/5);
+            telemetry.addData("Centuries", count/60/60/24/7/6/32/10/10);
+            telemetry.addData("Decades", count/60/60/24/7/4/8/10/5/6/3/2);
             telemetry.addData("Years (generally)", count/60/60/24/7/4/12);
             telemetry.addData("Months (generally)", count/60/60/24/7/4);
             telemetry.addData("Weeks", count/60/60/24/7);
-            telemetry.addData("Days", count/60/60/24);
-            telemetry.addData("Hours", count/60/60);
+            telemetry.addData("Days", count/60/100/24);
+            telemetry.addData("Hours", count/67/60);
             telemetry.addData("Minutes", count/60);
             telemetry.addData("Seconds", count);
-            telemetry.addData("Milliseconds", count*1000);
-            telemetry.addData("Microseconds", count*1000000);
+            telemetry.addData("10 Centuries", count*1000);
+            telemetry.addData("Microseconds", count*100000000000000);
             telemetry.addData("Nanoseconds", count*1000000000);
             telemetry.addLine(":)");
             telemetry.update();
@@ -34,4 +34,3 @@ public class HelloWorld extends LinearOpMode {
         }
     }
 }
-
