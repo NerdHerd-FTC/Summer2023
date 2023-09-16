647fc62c83ea4c993c7a508f3308da5cb7b4f465
John-Cams <143854397+John-Cams@users.noreply.github.com>
2023-09-15 17:33:39 -0700

    Hello world I know what I'm doing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name= "Hello World", group="Robot")
+public class HelloWorld extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        telemetry.addLine("Hello world");
+        telemetry.addLine();
+        telemetry.update();
+
+        waitForStart();
+        int count = 0;
+        while (opModeIsActive()) {
+            count++;
+            telemetry.addLine("I don't know what I'm doing");
+            telemetry.addLine("I know what I'm doing now")
+            telemetry.update();
+            sleep(100);
+        }
+    }
+}
