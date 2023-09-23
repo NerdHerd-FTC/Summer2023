0482fb3bf40dc0f0e99e418944421c3801a3d329
John-Cams <143854397+John-Cams@users.noreply.github.com>
2023-09-23 09:56:00 -0700

    There are errors in moveInSquare.java so don't use it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/moveInSquare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/moveInSquare.java
@@ -1,25 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
+//So much importings
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.RobotAutoDriveByEncoder_Linear;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+//These three import statements are have errors even though it is direct copy of the LearnJavaForFTC.pdf page 110
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 
 @Autonomous()
-public abstract class moveInSquare extends OpMode{
+public class moveInSquare extends OpMode{
 
-    private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
     @Override
     public void init() {
+        private AprilTagProcessor aprilTagProcessor;
+        private VisionPortal visionPortal;
         telemetry.addLine("Program Initialized");
         telemetry.update();
     }
 
+    //This had an error earlier for some reason but
+    //now doesn't because of the abstract next to the public class
+    @Override
     public void loop() {
-        frontLeftMotor.setPower(1);
-        frontRightMotor.setPower(1);
+        if(gamepad1.a) {
+
+        }
+
     }
 }
